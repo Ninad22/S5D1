1a416b8857ef724c69edc13f478c8c10668e61a2
Ninad22 <nina.duval22@gmail.com>
2020-02-04 00:58:41 +0100

    ok

---
+++ README.md
@@ -0,0 +1,8 @@
+This is a program to add gossip of THP members to as CSV database.
+
+The added gossips can also be displayed.
+
+In addition each gossip has its own page/url.
+
+I finished the project at 2.6.5, so with this program is not possible to edit gossips.
+
---
+++ lib/gossip.rb
@@ -0,0 +1,39 @@
+require 'csv'
+class Gossip
+
+attr_reader :author, :content 
+    
+    
+  def initialize (author,content)
+    @author = author
+    @content = content
+  end
+
+
+  def save
+    CSV.open("./db/gossip.csv","ab") do |csv|
+     csv << [@author,@content]
+    end
+  end
+
+  def self.all
+    all_gossips = []
+    CSV.read("./db/gossip.csv").each do |csv_line|
+     all_gossips << Gossip.new(csv_line[0],csv_line[1])
+    end
+   return all_gossips
+  end
+
+
+   def self.find(id)
+    gossips = []
+    CSV.read("./db/gossip.csv").each_with_index do |row,index|
+       if id == index+1 #cherche et check si l'index est égale à l'id demandé
+        gossips << Gossip.new(row[0], row[1]) #si trouvé, ajout dans array et break pour retourner l'array
+       break    
+       end    
+      end    
+    return gossips   
+   end
+end
+
